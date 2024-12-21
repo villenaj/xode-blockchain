df105e2373b04c9f82f476c3e3a1f0c564331978
hgminerva <hgminerva@gmail.com>
2024-12-21 15:26:29 +0800

    Added pallet staking mock.rs (https://github.com/Xode-DAO/xode-blockchain/issues/29)

--- pallets/staking/src/mock.rs
+++ pallets/staking/src/mock.rs
@@ -1,6 +1,27 @@
-use frame_support::{derive_impl, weights::constants::RocksDbWeight};
-use frame_system::{mocking::MockBlock, GenesisConfig};
-use sp_runtime::{traits::ConstU64, BuildStorage};
+use frame_support::{derive_impl, 
+	weights::constants::RocksDbWeight,
+	PalletId,
+};
+use frame_system::{mocking::MockBlock, EnsureRoot, GenesisConfig};
+use sp_runtime::{traits::ConstU64, BuildStorage, impl_opaque_keys};
+use frame_support::parameter_types;
+use sp_consensus_aura::sr25519::AuthorityId as AuraId;
+use xcm::latest::prelude::BodyId;
+
+pub const SLOT_DURATION: u64 = 6000;
+pub type Balance = u128;
+pub type AccountId = u64;
+pub type BlockNumber = u32;
+
+pub const MILLI_SECS_PER_BLOCK: u32 = 6000;
+pub const MINUTES: BlockNumber = 60_000 / (MILLI_SECS_PER_BLOCK as BlockNumber);
+pub const HOURS: BlockNumber = MINUTES * 60;
+
+impl_opaque_keys! {
+	pub struct SessionKeys {
+		pub aura: Aura,
+	}
+}
 
 // Configure a mock runtime to test the pallet.
 #[frame_support::runtime]
@@ -21,8 +42,27 @@ mod test_runtime {
 
 	#[runtime::pallet_index(0)]
 	pub type System = frame_system;
+
 	#[runtime::pallet_index(1)]
-	pub type TemplateModule = crate;
+	pub type XodeStaking = crate;
+
+	#[runtime::pallet_index(2)]
+	pub type Balances = pallet_balances;
+
+	#[runtime::pallet_index(3)]
+	pub type Timestamp = pallet_timestamp;
+
+	#[runtime::pallet_index(4)]
+	pub type Aura = pallet_aura;
+
+	#[runtime::pallet_index(5)]
+	pub type CollatorSelection = pallet_collator_selection;
+
+	#[runtime::pallet_index(6)]
+	pub type Authorship = pallet_authorship;
+
+	#[runtime::pallet_index(7)]
+	pub type Session = pallet_session;
 }
 
 #[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
@@ -31,11 +71,108 @@ impl frame_system::Config for Test {
 	type Block = MockBlock<Test>;
 	type BlockHashCount = ConstU64<250>;
 	type DbWeight = RocksDbWeight;
+	type AccountData = pallet_balances::AccountData<Balance>;
+}
+
+parameter_types! {
+	pub const MinimumPeriod: u64 = 0;
+}
+impl pallet_timestamp::Config for Test {
+	type Moment = u64;
+	type OnTimestampSet = Aura;
+	type MinimumPeriod = MinimumPeriod;
+	type WeightInfo = ();
+}
+
+parameter_types! {
+	pub const MaxAuthorities: u32 = 1_000;
+	pub const AllowMultipleBlocksPerSlot: bool = true;
+}
+impl pallet_aura::Config for Test {
+	type AuthorityId = AuraId;
+	type DisabledValidators = ();
+	type MaxAuthorities = MaxAuthorities;
+	type AllowMultipleBlocksPerSlot = AllowMultipleBlocksPerSlot;
+	type SlotDuration = ConstU64<SLOT_DURATION>;
+}
+
+parameter_types! {
+	pub const PotId: PalletId = PalletId(*b"PotStake");
+	pub const SessionLength: BlockNumber = 6 * HOURS;
+	pub const StakingAdminBodyId: BodyId = BodyId::Defense;
+	pub const MaxCandidates: u32 = 100;
+	pub const MinEligibleCollators: u32 = 4;
+	pub const MaxInvulnerables: u32 = 20;
+}
+
+impl pallet_collator_selection::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type Currency = Balances;
+	type UpdateOrigin = EnsureRoot<AccountId>;
+	type PotId = PotId;
+	type MaxCandidates = MaxCandidates;
+	type MinEligibleCollators = MinEligibleCollators;
+	type MaxInvulnerables = MaxInvulnerables;
+	type KickThreshold = Period;
+	type ValidatorId = <Self as frame_system::Config>::AccountId;
+	type ValidatorIdOf = pallet_collator_selection::IdentityCollator;
+	type ValidatorRegistration = Session;
+	type WeightInfo = ();
+}
+
+parameter_types! {
+	pub const Period: u32 = MINUTES;
+	pub const Offset: u32 = 0;
+}
+
+impl pallet_session::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type ValidatorId = <Self as frame_system::Config>::AccountId;
+	type ValidatorIdOf = pallet_collator_selection::IdentityCollator;
+	type ShouldEndSession = pallet_session::PeriodicSessions<Period, Offset>;
+	type NextSessionRotation = pallet_session::PeriodicSessions<Period, Offset>;
+	type SessionManager = XodeStaking;
+	type SessionHandler = <SessionKeys as sp_runtime::traits::OpaqueKeys>::KeyTypeIdProviders;
+	type Keys = SessionKeys;
+	type WeightInfo = ();
+}
+
+impl pallet_authorship::Config for Test {
+	type FindAuthor = pallet_session::FindAccountFromAuthorIndex<Self, Aura>;
+	type EventHandler = (CollatorSelection,);
+}
+
+parameter_types! {
+	pub const ExistentialDeposit: u128 = 0;
+}
+impl pallet_balances::Config for Test {
+	type MaxReserves = ();
+	type ReserveIdentifier = [u8; 4];
+	type MaxLocks = ();
+	type Balance = Balance;
+	type RuntimeEvent = RuntimeEvent;
+	type DustRemoval = ();
+	type ExistentialDeposit = ExistentialDeposit;
+	type AccountStore = System;
+	type WeightInfo = ();
+	type RuntimeHoldReason = ();
+	type FreezeIdentifier = ();
+	type MaxFreezes = ();
+	type RuntimeFreezeReason = ();
+}
+
+parameter_types! {
+	pub const MaxProposedCandidates: u32 = 200;
+	pub const MaxProposedCandidateDelegates: u32 = 200;
 }
 
 impl crate::Config for Test {
 	type RuntimeEvent = RuntimeEvent;
 	type WeightInfo = ();
+	type MaxProposedCandidates = MaxProposedCandidates;
+	type MaxProposedCandidateDelegates = MaxProposedCandidateDelegates;
+	type XaverNodes = ();
+	type StakingCurrency = Balances;
 }
 
 // Build genesis storage according to the mock runtime.
