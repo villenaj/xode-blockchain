9f6bc35384d71f5effbc18759e4a44df220f077b
Arneil Paul Polican <policanarneilpaul@gmail.com>
2025-09-16 16:05:34 +0800

    Update main.rs

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,6 +1,7 @@
 //! Substrate Parachain Node Template CLI
 
 #![warn(missing_docs)]
+use sp_core::crypto::{Ss58AddressFormat, set_default_ss58_version};
 
 mod chain_spec;
 mod cli;
@@ -9,5 +10,6 @@ mod rpc;
 mod service;
 
 fn main() -> sc_cli::Result<()> {
+	set_default_ss58_version(Ss58AddressFormat::custom(280));
 	command::run()
 }
